82b9148521d0369ffa1d68a33a488476fd0c7822
Chad Burkins <cburkins@users.noreply.github.com>
2017-12-07 17:34:35 -0500

    routing works now

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-console.log("Beginning");
 
 // MODULE
 // ngRoute and ngResource are injected into my app
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-
 // ROUTES
-weatherApp.config(function($routeProvider) {
+weatherApp.config(function($locationProvider, $routeProvider) {
 
+//	$locationProvider.html5Mode({enabled: false, requireBase: false});
+	$locationProvider.hashPrefix('');
 	$routeProvider
-
 		.when('/', {
 			templateUrl: 'pages/home.html',
 			controller: 'homeController'
 		})
-
 		.when('/forecast', {
 			templateUrl: 'pages/forecast.html',
 			controller: 'forecastController'
-		})
-
-
+		});	
 });
 
 // CONTROLLERS
-
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.controller('homeController', ['$scope', '$location', function($scope, $location) {
+	console.info("homeController");
+	console.info($location.path());
 
 }]);
 
-
-weatherApp.controller('forecastController', ['$scope', function($scope) {
+weatherApp.controller('forecastController', ['$scope', '$location', function($scope, $location) {
+	console.info("forecastController");
+	console.info($location.path());
 
 }]);
