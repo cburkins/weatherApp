bdc3be1b093b54d08e5be5e76c388329190b8387
Chad Burkins <cburkins@users.noreply.github.com>
2017-12-10 15:00:56 -0500

    converted to zip code because openweathermap does not handle US cities all that well.  Display date and daytime temp, but not yet in a readable format

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ weatherApp.config(function($locationProvider, $routeProvider, $sceDelegateProvid
 		// Allow same origin resource loads.
 		'self',
 		// Allow loading from our assets domain.  Notice the difference between * and **.
-		'http://api.openweathermap.org/**'
+		'http://api.openweathermap.org/**', 
+		'http://www.zipcodeapi.com/rest/**'
 	]);
 
 	//	$locationProvider.html5Mode({enabled: false, requireBase: false});
@@ -28,57 +29,69 @@ weatherApp.config(function($locationProvider, $routeProvider, $sceDelegateProvid
 // SERVICES
 
 // Service that shares the desired city between the two pages (controllers)
-weatherApp.service('cityService', function() {
-	this.city = "New York, NY";
+weatherApp.service('zipService', function() {
+	this.zipCode = "08869";
 });
 
 
 // CONTROLLERS
 
-weatherApp.controller('homeController', ['$scope', 'cityService', '$location',
+weatherApp.controller('homeController', ['$scope', 'zipService', '$location',
 	// Needs to be in same order as in brackets, protects against minification
-	function($scope, cityService, $location) {
+	function($scope, zipService, $location) {
 		console.log("Hello from Home Controller");
 		console.info("homeController");
 		console.info($location.path());
 
-		// Get the value of city from the cityService service
-		$scope.city = cityService.city;
+		// Get the value of city from the zipService service
+		$scope.zipCode = zipService.zipCode;
 
-		// Watch for when the 'city' value gets updated, and then update the cityService service
+		// Watch for when the 'city' value gets updated, and then update the zipService service
 		$scope.$watch('city', function() {
-			cityService.city = $scope.city;
+			zipService.zipCode = $scope.zipCode;
 		})
 
 	}
 ]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$sce', 'cityService', '$location',
+weatherApp.controller('forecastController', ['$scope', '$resource', '$sce', 'zipService', '$location',
 	// Needs to be in same order as in brackets, protects against minification
-	function($scope, $resource, $sce, cityService, $location) {
+	function($scope, $resource, $sce, zipService, $location) {
 		console.info("forecastController");
 		console.info($location.path());
 
-		// Get the value of city from the cityService service
-		$scope.city = cityService.city;
-		var theURL = "http://api.openweathermap.org/data/2.5/forecast/daily";
+		// Get the value of city from the zipService service
+		$scope.zipCode = zipService.zipCode;
 
-		// $scope.weatherAPI = $resource(theURL, {
-		// 	callback: "JSON_CALLBACK"
-		// }, {
-		// 	get: {
-		// 		method: "JSONP"
-		// 	}
-		// });
+		var zipURL="http://www.zipcodeapi.com/rest/"
+		// This key was registered to accept calls from amazonaws.com, so it won't cause CORS error
+		var zipAPI="js-jqHy5GgRmYUdo3hYO94BWVdYBP8XdP1gpBi8LNHJ2T2wef1x5gEOGehjIcUT8JJT"
+		var zipFunction="/city-zips.json"
+		var city = "philadelphia"
+		var state = "pa"
+		var zipCall = zipURL + zipAPI + zipFunction + "/" + city + "/" + state
+		console.log(zipCall);
+		// The call should look something like this
+		// https://www.zipcodeapi.com/rest/js-jqHy5GgRmYUdo3hYO94BWVdYBP8XdP1gpBi8LNHJ2T2wef1x5gEOGehjIcUT8JJT/city-zips.json/philadelphia/pa");
+		// --------------------------------------------------------
+		$scope.zipAPI = $resource(zipCall);
+
+		$scope.zipResult = $scope.zipAPI.get({}, function () {
+			console.log($scope.zipResult.zip_codes[0])
+		});
+		console.warn($scope.zipResult);
+
+		// --------------------------------------------------------
 
 
-		$scope.weatherAPI = $resource(theURL);
-
+		$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily");
 		$scope.weatherResult = $scope.weatherAPI.get({
-			zip: $scope.city,
+			zip: $scope.zipCode,
 			cnt: 2,
 			APPID: "cbf71841c266028edf05411b46d61152"
 		});
+
+
 		console.log($scope.weatherResult);
 
 	}
